ddc05fa4a246ea841e9955b2021b0addcc59dda9
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-02 22:04:02 +0600

    your comment

---
+++ array-loop-element.js
@@ -0,0 +1,34 @@
+// array looping system
+
+var Numbers = [12, 23, 54, 87, 69, 45, 1, 25, 46, 33];
+
+for (var i = 0; i < 10; i++) {
+
+    var element = Numbers[i]
+
+    console.log(element);
+}
+
+
+
+
+// zodi amader i<infinite hoy ba onek beshi hoy tahole var name.length dile seita autometic length niye nibe 
+var Numbers = [12, 23, 54, 87, 69, 45, 1, 25, 46, 33, 45, 46, 47];
+for (var i = 0; i < Numbers.length; i++) {
+    var element = Numbers[i]
+    console.log(element);
+}
+
+
+
+
+
+
+
+// complex array loop.........
+
+var Name = ['sakib', 'rakib', 'makib', 'takib', 'taskin', 'juju'];
+for (i = 0; i < Name.length; i++) {
+    var newName = Name[i];
+    console.log(newName);
+}
