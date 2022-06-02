55c2113b3ef838f1771811600b1f43e8b977b227
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-02 21:33:03 +0600

    first commit

---
+++ for-loop.js
@@ -0,0 +1,25 @@
+
+// part-01
+
+for (var rostGiven = 0; rostGiven < 7; rostGiven++) {
+    console.log('rost den please');
+    console.log(rostGiven);
+}
+
+
+
+// part-02
+// Odd Number
+
+
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+    i = i + 1;
+}
+
+
+// even number
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+    i = i + 1;
+}
